cb346b281bb7c0cbc871dcefa5273f5605256225
Daniel Vargas <danielbond09@hotmail.com>
2017-07-21 21:16:04 -0500

    Botón para rentar funcional. Mejoras a la DB.

--- database.py
+++ database.py
@@ -6,23 +6,183 @@ def inicializarBase():
 	conexion = sqlite3.connect('departamentosVH.db')
 	cursor = conexion.cursor()
 	try:
-	    cursor.execute('''CREATE TABLE departamento (
-	    			numeroDepto 		integer primary key, 
-	                nombreInquilino 	text, 
-	                telefonoInquilino 	integer, 
-	                procedenciaInquilino text,
-	                tipoIdentificacion 	text,
-	                numeroPersonas 		integer,
-	                costo 				real,
-	                tipoReservacion 	text,
-	                depositoReservacion	real,
-	                depositoGarantia	real,
-	                pagaLuz				integer, 	#Convertir a booleano
-	                fechaEntrada		text,		#Convertir a fecha
-	                fechaSalida			text,		#Convertir a fecha
-	                ocupado				integer,	#Convertir a booleano
-	                observaciones		text
-	                )''')
-	except:
-	    print("No fue necesario recrear la base.")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento1 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento3 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento5 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento6 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento7 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento8 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento9 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento10 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento11 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento12 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	    cursor.execute("""CREATE TABLE IF NOT EXISTS departamento13 (
+	    			numeroDepto 		INTEGER PRIMARY KEY, 
+	                nombreInquilino 	TEXT, 
+	                telefonoInquilino 	INTEGER, 
+	                procedenciaInquilino TEXT,
+	                tipoIdentificacion 	TEXT,
+	                numeroPersonas 		INTEGER,
+	                costo 				REAL,
+	                tipoReservacion 	TEXT,
+	                deposito			REAL,
+	                pagaLuz				INTEGER, 	
+	                fechaEntrada		TEXT,		
+	                fechaSalida			TEXT,		
+	                ocupado				INTEGER,	
+	                observaciones		TEXT
+	                )""")
+	except Exception as ex:
+	    print("Ocurrio el siguiente error al crear la base:")
+	    print(ex)
 	conexion.close()
--- guis.py
+++ guis.py
@@ -5,6 +5,7 @@ from tkinter import *    # Carga módulo tk (widgets estándar)
 from tkinter import ttk  # Carga ttk (para widgets nuevos 8.5+)
 from departamento import *
 from database import *
+import datetime
 
 #Clase que define la ventana principal de la aplicacion
 class VistaPrincipal():
@@ -64,88 +65,95 @@ class VistaRentar():
 
 		#Widgets para ingresar nombre.
 		Label(self.ventanaRentar, text = "Nombre: ", font = "Default 14", anchor="nw").grid(row = 1, column = 0, sticky="e")
-		eNombre = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
-		eNombre.grid(row = 1, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		self.eNombre = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
+		self.eNombre.grid(row = 1, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
 
 		#Widgets para ingresar teléfono.
 		Label(self.ventanaRentar, text = "Teléfono: ", font = "Default 14").grid(row = 2, column = 0, sticky="e")
-		eTelefono = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
-		eTelefono.grid(row = 2, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		self.eTelefono = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
+		self.eTelefono.grid(row = 2, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
 
 		#Widgets para ingresar procedencia.
 		Label(self.ventanaRentar, text = "Procedencia: ", font = "Default 14").grid(row = 3, column = 0, sticky="e")
-		eProcedencia = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
-		eProcedencia.grid(row = 3, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		self.eProcedencia = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
+		self.eProcedencia.grid(row = 3, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
 
 		#Widgets para ingresar tipo de identificación.
 		Label(self.ventanaRentar, text = "Identificación: ", font = "Default 14").grid(row = 4, column = 0, sticky="e")
-		eIdentificacion = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
-		eIdentificacion.grid(row = 4, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		self.eIdentificacion = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
+		self.eIdentificacion.grid(row = 4, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
 
 		#Widgets para ingresar cantidad de personas.
 		Label(self.ventanaRentar, text = "Personas: ", font = "Default 14").grid(row = 5, column = 0, sticky="e")
-		ePersonas = Spinbox(self.ventanaRentar, from_ = 1, to = 10, font = "Default 12", width = 5)
-		ePersonas.grid(row = 5, column = 1, columnspan = 6, padx=(0,10), sticky="W")
+		self.ePersonas = Spinbox(self.ventanaRentar, from_ = 1, to = 10, font = "Default 12", width = 5)
+		self.ePersonas.grid(row = 5, column = 1, columnspan = 6, padx=(0,10), sticky="W")
 
 		#Widgets para ingresar costo.
 		Label(self.ventanaRentar, text = "Costo: ", font = "Default 14").grid(row = 6, column = 0, sticky="e")
-		eCosto = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
-		eCosto.grid(row = 6, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		self.eCosto = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
+		self.eCosto.grid(row = 6, column = 1, columnspan = 5, padx=(0,10), sticky="EW")
+		Label(self.ventanaRentar, text = "MXN", font = "Default 14").grid(row = 6, column = 6, sticky="w")
 
 		#Widgets para ingresar tipo de reservación.
 		Label(self.ventanaRentar, text = "Reservación por: ", font = "Default 14").grid(row = 7, column = 0, sticky="e")
-		eTipoReservacion = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 10)
-		eTipoReservacion["values"] = ["Día", "Semana", "Mes"]
-		eTipoReservacion.grid(row = 7, column = 1, columnspan = 6, padx=(0,10), sticky="W")
-		eTipoReservacion.current(0)
+		self.eTipoReservacion = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 10)
+		self.eTipoReservacion["values"] = ["Día", "Semana", "Mes"]
+		self.eTipoReservacion.grid(row = 7, column = 1, columnspan = 6, padx=(0,10), sticky="W")
+		self.eTipoReservacion.current(0)
 
 		#Widgets para ingresar cantidad del depósito.
 		Label(self.ventanaRentar, text = "Depósito: ", font = "Default 14").grid(row = 8, column = 0, sticky="e")
-		eDeposito = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
-		eDeposito.grid(row = 8, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		self.eDeposito = ttk.Entry(self.ventanaRentar, font = "Default 12", width = 8)
+		self.eDeposito.grid(row = 8, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
 
 		#Widgets para ingresar si el inquilino paga su luz.
 		Label(self.ventanaRentar, text = "¿Paga su luz? ", font = "Default 14").grid(row = 9, column = 0, sticky="e")
-		ePagaLuz = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
-		ePagaLuz["values"] = ["No", "Sí"]
-		ePagaLuz.grid(row = 9, column = 1, columnspan = 6, padx=(0,10), sticky="W")
-		ePagaLuz.current(0)
+		self.ePagaLuz = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
+		self.ePagaLuz["values"] = ["No", "Sí"]
+		self.ePagaLuz.grid(row = 9, column = 1, columnspan = 6, padx=(0,10), sticky="W")
+		self.ePagaLuz.current(0)
 
 		#Widgets para ingresar fecha de entrada del inquilino.
 		Label(self.ventanaRentar, text = "Fecha de entrada: ", font = "Default 14").grid(row = 10, column = 0, sticky="e")
 		Label(self.ventanaRentar, text = "Día: ", font = "Default 12").grid(row = 10, column = 1, sticky="e")
-		eDiaEntrada = Spinbox(self.ventanaRentar, from_ = 1, to = 31, font = "Default 12", width = 5)
-		eDiaEntrada.grid(row = 10, column = 2, sticky="w", padx=(0,10))
+		self.eDiaEntrada = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
+		self.eDiaEntrada["values"] = list(range(2000, 2100))
+		self.eDiaEntrada.grid(row = 10, column = 2, columnspan = 1, padx=(0,10), sticky="W")
+		self.eDiaEntrada.current(17)
 		Label(self.ventanaRentar, text = "Mes: ", font = "Default 12").grid(row = 10, column = 3, sticky="e")
-		eMesEntrada = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
-		eMesEntrada["values"] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-		eMesEntrada.grid(row = 10, column = 4, columnspan = 1, padx=(0,10), sticky="W")
-		eMesEntrada.current(0)
+		self.eMesEntrada = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
+		self.eMesEntrada["values"] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+		self.eMesEntrada.grid(row = 10, column = 4, columnspan = 1, padx=(0,10), sticky="W")
+		self.eMesEntrada.current(0)
 		Label(self.ventanaRentar, text = "Año: ", font = "Default 12").grid(row = 10, column = 5, sticky="e")
-		eAnioEntrada = Spinbox(self.ventanaRentar, from_ = 2017, to = 2100, font = "Default 12", width = 5)
-		eAnioEntrada.grid(row = 10, column = 6, sticky="w", padx=(0,10))
+		self.eAnioEntrada = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
+		self.eAnioEntrada["values"] = list(range(2000, 2100))
+		self.eAnioEntrada.grid(row = 10, column = 6, columnspan = 1, padx=(0,10), sticky="W")
+		self.eAnioEntrada.current(17)
 
 		#Widgets para ingresar fecha de salida del inquilino.
 		Label(self.ventanaRentar, text = "Fecha de Salida: ", font = "Default 14").grid(row = 11, column = 0, sticky="e")
 		Label(self.ventanaRentar, text = "Día: ", font = "Default 12").grid(row = 11, column = 1, sticky="e")
-		eDiaEntrada = Spinbox(self.ventanaRentar, from_ = 1, to = 31, font = "Default 12", width = 5)
-		eDiaEntrada.grid(row = 11, column = 2, sticky="w", padx=(0,10))
+		self.eDiaSalida = Spinbox(self.ventanaRentar, from_ = 1, to = 31, font = "Default 12", width = 5)
+		self.eDiaSalida.grid(row = 11, column = 2, sticky="w", padx=(0,10))
 		Label(self.ventanaRentar, text = "Mes: ", font = "Default 12").grid(row = 11, column = 3, sticky="e")
-		eMesEntrada = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
-		eMesEntrada["values"] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
-		eMesEntrada.grid(row = 11, column = 4, columnspan = 1, padx=(0,10), sticky="W")
-		eMesEntrada.current(0)
+		self.eMesSalida = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
+		self.eMesSalida["values"] = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"]
+		self.eMesSalida.grid(row = 11, column = 4, columnspan = 1, padx=(0,10), sticky="W")
+		self.eMesSalida.current(0)
 		Label(self.ventanaRentar, text = "Año: ", font = "Default 12").grid(row = 11, column = 5, sticky="e")
-		eAnioEntrada = Spinbox(self.ventanaRentar, from_ = 2017, to = 2100, font = "Default 12", width = 5)
-		eAnioEntrada.grid(row = 11, column = 6, sticky="w", padx=(0,10))
+		self.eAnioSalida = ttk.Combobox(self.ventanaRentar, state="readonly", font = "Default 12", width = 5)
+		self.eAnioSalida["values"] = list(range(2000, 2100))
+		self.eAnioSalida.grid(row = 11, column = 6, columnspan = 1, padx=(0,10), sticky="W")
+		self.eAnioSalida.current(17)
 
 		#Widgets para ingresar observaciones.
 		Label(self.ventanaRentar, text = "Observaciones: ", font = "Default 14").grid(row = 12, column = 0, sticky="e")
-		eObservaciones = Text(self.ventanaRentar, height = 3, width = 8, font = "Default 12")
-		eObservaciones.grid(row = 12, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
-		scroll = Scrollbar(self.ventanaRentar, command=eObservaciones.yview)
+		self.eObservaciones = Text(self.ventanaRentar, height = 3, width = 8, font = "Default 12")
+		self.eObservaciones.grid(row = 12, column = 1, columnspan = 6, padx=(0,10), sticky="EW")
+		scroll = Scrollbar(self.ventanaRentar, command=self.eObservaciones.yview)
 		scroll.grid(row = 12, column = 1, columnspan = 6, padx=(0,10), sticky="E")
-		eObservaciones['yscrollcommand'] = scroll.set
+		self.eObservaciones['yscrollcommand'] = scroll.set
 
 		#Botones de control
 		ttk.Button(self.ventanaRentar, text = 'Regresar', command = self.regresar).grid(row = 13, column = 2, sticky="e")
@@ -158,10 +166,37 @@ class VistaRentar():
 		self.ventanaRentar.destroy()
 		VistaPrincipal()
 
-	#Método que adquiere todos los valores de los widgets y los almacena en la base de datos.
 	def guardarDatos(self):
+		conexion = sqlite3.connect("departamentosVH.db")
+		cursor = conexion.cursor()
+		fechaEntrada = self.eDiaEntrada.get() + str(self.eMesEntrada.current() + 1) + self.eAnioEntrada.get()
+		fechaSalida = self.eDiaSalida.get() + str(self.eMesSalida.current() + 1) + self.eAnioSalida.get()
+		try:
+			cursor.execute("""INSERT INTO departamento%d 
+				VALUES(NULL, '%s', %d, '%s', '%s', %d, %f, '%s', %f, %d, '%s', '%s', %d, '%s')""" %(
+					self.numeroDepto,
+					self.eNombre.get(), 
+					int(self.eTelefono.get()), 
+					self.eProcedencia.get(), 
+					self.eIdentificacion.get(),
+					int(self.ePersonas.get()),
+					float(self.eCosto.get()),
+					self.eTipoReservacion.get(), 
+					float(self.eDeposito.get()),
+					int(self.ePagaLuz.current()),
+					fechaEntrada,
+					fechaSalida,
+					1, 
+					"Blablabla")
+				)
+			conexion.commit()
+			conexion.close()
+		except Exception as ex:
+			print("Ocurrió el siguiente error al guardar en la base de datos:")
+			print(ex)
+			conexion.rollback()
+
 		self.ventanaRentar.destroy()
-		#Poner los leido de los cuadros de texto (NombreCT.get())
 		VistaPrincipal()
 
 
@@ -185,6 +220,25 @@ class VistaHistorial():
 		Label(self.ventanaHistorial, text = "Costo", font = "Default 12 bold").grid(row = 1, column = 5)
 		Label(self.ventanaHistorial, text = "Tipo Res.", font = "Default 12 bold").grid(row = 1, column = 6)
 		Label(self.ventanaHistorial, text = "Depósito", font = "Default 12 bold").grid(row = 1, column = 7)
+		#Se inicializa el widget para desplegar historial
+		self.wHistorial = Text(self.ventanaHistorial, height = 20, width = 8, font = "Default 12")
+		self.wHistorial.grid(row = 2, column = 0, columnspan = 8, padx=(0,10), sticky="EW")
+		scroll = Scrollbar(self.ventanaHistorial, command=self.wHistorial.yview)
+		scroll.grid(row = 2, column = 1, columnspan = 8, padx=(0,10), sticky="NSE")
+		self.wHistorial['yscrollcommand'] = scroll.set
+		#Se leen los datos de la base de datos
+		conexion = sqlite3.connect("departamentosVH.db")
+		cursor = conexion.cursor()
+		try:
+			cursor.execute("SELECT * FROM departamento%d ORDER BY numeroDepto DESC" %(self.numeroDepto)) 
+			historial = str(cursor.fetchall())
+		except Exception as ex:
+			print("Ocurrió el siguiente error al leer de la base de dat:")
+			print(ex)
+			conexion.rollback()
+		else: #Si se pudo leer, se pone en el widget de texto.
+			#HAY QUE DARLE MAS FORMATO A ESTA IMPRESION
+			self.wHistorial.insert(INSERT, historial)
 		centrarVentana(self.ventanaHistorial)
 		self.ventanaHistorial.mainloop()
 	
